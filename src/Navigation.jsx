620e65c56846675c055bf571868e767976fb1654
PhilArceno <54872569+PhilArceno@users.noreply.github.com>
2020-01-18 14:53:46 -0500

    desktop nav css

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-<<<<<<< HEAD
 
 class Navigation extends Component{
     render = () => {    
@@ -22,30 +21,6 @@ class Navigation extends Component{
             </ul>
         </div>)
     }     
-=======
-class Navigation extends Component {
-  render = () => {
-    return (
-      <div className="desktopnav">
-        <div>Navigation</div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/add-item">Add item</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  };
->>>>>>> db03cac0c1ccbdbceaa79b38a308310eea4570a3
 }
 
 export default Navigation;
