6d1223b493548d61c7464df013ee1dec1f3463d3
teresa <teresa.s.lacroix@gmail.com>
2020-01-21 22:01:11 -0500

    registration page

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,47 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
 
 class Register extends Component {
-    constructor() { 
-        super() 
-        this.state = {
-            usernameRegisterInput: "",
-            passwordRegisterInput: "", 
-        } 
-    } 
-    usernameRegisterChange = evt => {
-        this.setState({ usernameRegisterInput: evt.target.value })
-    }
-    passwordRegisterChange = evt => {
-        this.setState({ passwordRegisterInput: evt.target.value })
-    }
+  constructor() {
+    super();
+    this.state = {
+      usernameRegisterInput: '',
+      passwordRegisterInput: ''
+    };
+  }
+  usernameRegisterChange = evt => {
+    this.setState({ usernameRegisterInput: evt.target.value });
+  };
+  passwordRegisterChange = evt => {
+    this.setState({ passwordRegisterInput: evt.target.value });
+  };
 
-    submitRegisterHandler = async evt => {
-        evt.preventDefault()
-        let name = this.state.usernameRegisterInput
-        let data = new FormData()
-        data.append("username", name)
-        data.append("password", this.state.passwordRegisterInput)
-        let response = await fetch('/register', { method: "POST", body: data })
-        let body = await response.text()
-        console.log("/register response", body)
-        body = JSON.parse(body)
-        if (body.success) {
-            this.setState({ 
-                usernameRegisterInput: "",
-                passwordRegisterInput: "" })
-        } 
-
+  submitRegisterHandler = async evt => {
+    evt.preventDefault();
+    let name = this.state.usernameRegisterInput;
+    let data = new FormData();
+    data.append('username', name);
+    data.append('password', this.state.passwordRegisterInput);
+    let response = await fetch('/register', { method: 'POST', body: data });
+    let body = await response.text();
+    console.log('/register response', body);
+    body = JSON.parse(body);
+    if (body.success) {
+      this.setState({
+        usernameRegisterInput: '',
+        passwordRegisterInput: ''
+      });
     }
-    render = () => {
-        return (
-        <form onSubmit={this.submitRegisterHandler}>
+  };
+  render = () => {
+    return (
+      <div className="registration-container">
+        <div className="registration-form-container">
+          <Link to="/">
+            <h1>AliBay</h1>
+          </Link>
+          <form onSubmit={this.submitRegisterHandler}>
             <h2>Register</h2>
-            <div>Username <input type="text" onChange={this.usernameRegisterChange} value={this.state.usernameRegisterInput}/> </div>
-            <div>Password <input type="text" onChange={this.passwordRegisterChange} value={this.state.passwordRegisterInput}/> </div>
-            <input type="submit" value="register" />
-        </form>
-        )
-    }
+            <div>
+              <input
+                className="registration-form-input"
+                type="text"
+                onChange={this.usernameRegisterChange}
+                value={this.state.usernameRegisterInput}
+                placeholder="Username"
+              />{' '}
+            </div>
+            <div>
+              <input
+                className="registration-form-input"
+                type="text"
+                onChange={this.passwordRegisterChange}
+                value={this.state.passwordRegisterInput}
+                placeholder="Password"
+              />{' '}
+            </div>
+            <button className="registration-botton">Sign up</button>
+            <Link to="/login">
+              <div className="registration-login-redirect">
+                Already a user? <span>Log in</span>
+              </div>
+            </Link>
+          </form>
+        </div>
+        <div className="registration-image1-container">
+          <img
+            src="../uploads/register-image1.webp"
+            className="registration-image1"
+          />
+        </div>
+      </div>
+    );
+  };
 }
 
-export default Register
+export default Register;
