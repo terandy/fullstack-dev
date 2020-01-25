b6e669fb9cdfdbe6b58911bf29c42114c5ad6959
teresa <teresa.s.lacroix@gmail.com>
2020-01-24 22:50:34 -0500

    on hover image change

--- src/OneItem.jsx
+++ src/OneItem.jsx
@@ -4,13 +4,30 @@ import { Route, BrowserRouter, Link } from 'react-router-dom';
 class OneItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imageIndex: 0
+    };
   }
+  toggleImage = () => {
+    if (this.state.imageIndex === 0) {
+      this.setState({ imageIndex: 1 });
+      return;
+    } else {
+      this.setState({ imageIndex: 0 });
+      return;
+    }
+  };
+
   render = () => {
     return (
       <Link to={'/item-detail/' + this.props.item._id}>
         <div className="one-item">
-          <div className="one-item-image">
-            <img src={'..' + this.props.item.imgPaths[0]} />
+          <div
+            onMouseEnter={this.toggleImage}
+            onMouseLeave={this.toggleImage}
+            className="one-item-image"
+          >
+            <img src={'..' + this.props.item.imgPaths[this.state.imageIndex]} />
           </div>
           <div className="one-item-text">
             <h1>{this.props.item.item}</h1>
